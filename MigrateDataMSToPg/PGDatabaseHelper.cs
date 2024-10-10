4334e1ec318f5469eab72d11b93bf0f572913942
Ivan Kolganov <ikolganov@whitebox.ru>
2024-10-10 21:56:06 +0300

    added for bit

--- MigrateDataMSToPg/PGDatabaseHelper.cs
+++ MigrateDataMSToPg/PGDatabaseHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
+using System.Text;
 using Npgsql;
 
 namespace MigrateDataMSToPg;
@@ -64,143 +65,65 @@ public class PGDatabaseHelper
         }
     }
 
-    public static void BulkInsertIntoPostgreSQL(NpgsqlConnection pgConnection, SqlConnection msConn, string tableSchema,
-        string tableName,
-        List<(string ColumnName, string DataType)> filteredColumns, DataTable dataTable)
+public static void BulkInsertIntoPostgreSQL(NpgsqlConnection pgConnection, SqlConnection msConn, string tableSchema, 
+    string tableName, List<(string ColumnName, string DataType)> filteredColumns, DataTable dataTable)
+{
+    try
     {
-        try
+        int batchSize = 1000; // Размер батча для пакетной вставки
+        int rowCount = dataTable.Rows.Count;
+
+        // Формируем строку с именами столбцов
+        string columnNames = GetColumnNames(filteredColumns);
+
+        // Проверяем, существует ли таблица
+        if (!TableExists(pgConnection, tableName, tableSchema))
         {
-            int batchSize = 1000; // Размер батча для пакетной вставки
-            int rowCount = dataTable.Rows.Count;
-
-            // Формируем строку с именами столбцов
-            string columnNames = string.Join(", ", filteredColumns.ConvertAll(c =>
-                c.ColumnName.Equals("User", StringComparison.OrdinalIgnoreCase)
-                    ? "\"user\""
-                    : c.ColumnName.Equals("Order", StringComparison.OrdinalIgnoreCase)
-                        ? "\"order\""
-                        : c.ColumnName.Equals("Description", StringComparison.OrdinalIgnoreCase)
-                            ? "\"description\""
-                            : c.ColumnName.Equals("From", StringComparison.OrdinalIgnoreCase)
-                                ? "\"from\""
-                                : c.ColumnName.Equals("Group", StringComparison.OrdinalIgnoreCase)
-                                    ? "\"group\""
-                                    : c.ColumnName.Equals("Table", StringComparison.OrdinalIgnoreCase)
-                                        ? "\"table\""
-                                        : $"\"{c.ColumnName.ToLower()}\""));
-
-            // Проверяем, существует ли таблица
-            if (!TableExists(pgConnection, tableName, tableSchema))
-            {
-                Console.WriteLine($"Таблица '{tableName}' не найдена в базе данных PostgreSQL.");
-                return; // Выходим, если таблицы нет
-            }
-
-            int msCount = GetRowCountInMsSql(msConn, tableName);
-            int pgCount = GetRowCountInPostgreSQL(pgConnection, tableSchema, tableName);
-
-            Console.WriteLine($"Таблица: {tableName} - Количество записей в MSSQL: {msCount}, в PostgreSQL: {pgCount}");
-
-            // Проверяем, если в PostgreSQL больше или столько же записей, пропускаем таблицу
-            if (pgCount >= msCount)
-            {
-                Console.WriteLine($"Пропуск таблицы '{tableName}' — записи уже существуют.");
-                return;
-            }
-
-            using (var transaction = pgConnection.BeginTransaction()) // Используем транзакцию
-            {
-                try
-                {
-                    for (int i = 0; i < rowCount; i += batchSize)
-                    {
-                        int currentBatchSize = Math.Min(batchSize, rowCount - i);
-                        var insertQuery = new System.Text.StringBuilder(
-                            $"INSERT INTO {tableSchema}.\"{tableName.ToLower()}\" ({columnNames}) OVERRIDING SYSTEM VALUE VALUES ");
-
-                        // Собираем несколько строк данных в один запрос
-                        var allRows = new List<string>();
-                        for (int j = i; j < i + currentBatchSize; j++)
-                        {
-                            var rowValues = new List<string>();
-                            foreach (var column in filteredColumns)
-                            {
-                                var parameterValue = dataTable.Rows[j][column.ColumnName];
-
-                                // Преобразование строк и UUID
-                                if (parameterValue is string || parameterValue is Guid)
-                                {
-                                    rowValues.Add(
-                                        $"'{parameterValue.ToString().Replace("'", "''")}'"); // Заключаем в кавычки
-                                }
-                                // Преобразуем дату в нужный формат
-                                else if (parameterValue is DateTime dateTimeValue)
-                                {
-                                    rowValues.Add($"'{dateTimeValue.ToString("yyyy-MM-dd HH:mm:ss")}'");
-                                }
-                                // Если тип данных - timestamp, ставим NULL
-                                else if (column.DataType == "timestamp")
-                                {
-                                    rowValues.Add("NULL");
-                                }
-                                // Преобразование для типа time
-                                else if (column.DataType == "time")
-                                {
-                                    rowValues.Add($"'{parameterValue.ToString()}'");  // Формат для поля типа time
-                                }
-                                // Преобразование bit в целое
-                                else if (column.DataType == "bit")
-                                {
-                                    rowValues.Add(parameterValue != DBNull.Value && (bool)parameterValue ? "1" : "0");
-                                }
-                                // Обработка числовых значений с точкой как разделителем дробной части
-                                else if (parameterValue is double || parameterValue is float ||
-                                         parameterValue is decimal)
-                                {
-                                    rowValues.Add(Convert.ToString(parameterValue,
-                                        System.Globalization.CultureInfo.InvariantCulture));
-                                }
-                                // Обрабатываем NULL значения
-                                else if (parameterValue == DBNull.Value)
-                                {
-                                    rowValues.Add("NULL");
-                                }
-                                else
-                                {
-                                    rowValues.Add(parameterValue.ToString());
-                                }
-                            }
-
-                            allRows.Add($"({string.Join(", ", rowValues)})");
-                        }
-
-                        insertQuery.Append(string.Join(", ", allRows));
-                        insertQuery.Append(";");
-
-                        // Выполняем пакетный запрос
-                        using (var cmd = new NpgsqlCommand(insertQuery.ToString(), pgConnection))
-                        {
-                            Console.WriteLine(insertQuery.ToString());
-                            cmd.ExecuteNonQuery();
-                        }
-
-                        Console.WriteLine($"Batch of {currentBatchSize} rows inserted into {tableName}.");
-                    }
-
-                    transaction.Commit(); // Коммит транзакции
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Error during bulk insert: {ex.Message}");
-                    transaction.Rollback(); // Откат транзакции в случае ошибки
-                }
-            }
+            Console.WriteLine($"Таблица '{tableName}' не найдена в базе данных PostgreSQL.");
+            return; // Выходим, если таблицы нет
         }
-        catch (Exception ex)
+
+        int msCount = GetRowCountInMsSql(msConn, tableName);
+        int pgCount = GetRowCountInPostgreSQL(pgConnection, tableSchema, tableName);
+
+        Console.WriteLine($"Таблица: {tableName} - Количество записей в MSSQL: {msCount}, в PostgreSQL: {pgCount}");
+
+        // Проверяем, если в PostgreSQL больше или столько же записей, пропускаем таблицу
+        if (pgCount >= msCount)
         {
-            Console.WriteLine($"Error during bulk insert: {ex.Message}");
+            Console.WriteLine($"Пропуск таблицы '{tableName}' — записи уже существуют.");
+            return;
+        }
+
+        using (var transaction = pgConnection.BeginTransaction()) // Используем транзакцию
+        {
+            try
+            {
+                for (int i = 0; i < rowCount; i += batchSize)
+                {
+                    int currentBatchSize = Math.Min(batchSize, rowCount - i);
+                    var insertQuery = PrepareInsertQuery(filteredColumns, tableSchema, tableName, dataTable, i, currentBatchSize);
+
+                    ExecuteBatchInsert(pgConnection, insertQuery);
+
+                    Console.WriteLine($"Batch of {currentBatchSize} rows inserted into {tableName}.");
+                }
+
+                transaction.Commit(); // Коммит транзакции
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error during bulk insert: {ex.Message}");
+                transaction.Rollback(); // Откат транзакции в случае ошибки
+            }
         }
     }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error during bulk insert: {ex.Message}");
+    }
+}
+
 
 // Метод для проверки, существует ли таблица в базе данных PostgreSQL
     public static bool TableExists(NpgsqlConnection conn, string tableName, string tableSchema)
@@ -236,5 +159,118 @@ public class PGDatabaseHelper
             return (int)rowCount; // Приводим long к int, но будьте осторожны с большими значениями
         }
     }
+    
+    private static string GetColumnNames(List<(string ColumnName, string DataType)> filteredColumns)
+    {
+        return string.Join(", ", filteredColumns.ConvertAll(c =>
+            c.ColumnName.Equals("User", StringComparison.OrdinalIgnoreCase)
+                ? "\"user\""
+                : c.ColumnName.Equals("Order", StringComparison.OrdinalIgnoreCase)
+                    ? "\"order\""
+                    : c.ColumnName.Equals("Description", StringComparison.OrdinalIgnoreCase)
+                        ? "\"description\""
+                        : c.ColumnName.Equals("From", StringComparison.OrdinalIgnoreCase)
+                            ? "\"from\""
+                            : c.ColumnName.Equals("Group", StringComparison.OrdinalIgnoreCase)
+                                ? "\"group\""
+                                : c.ColumnName.Equals("Table", StringComparison.OrdinalIgnoreCase)
+                                    ? "\"table\""
+                                    : $"\"{c.ColumnName.ToLower()}\""));
+    }
+    
+    private static StringBuilder PrepareInsertQuery(List<(string ColumnName, string DataType)> filteredColumns, string tableSchema, 
+        string tableName, DataTable dataTable, int startIndex, int batchSize)
+    {
+        var insertQuery = new StringBuilder($"INSERT INTO {tableSchema}.\"{tableName.ToLower()}\" ({GetColumnNames(filteredColumns)}) OVERRIDING SYSTEM VALUE VALUES ");
 
+        var allRows = new List<string>();
+        for (int j = startIndex; j < startIndex + batchSize; j++)
+        {
+            var rowValues = PrepareRowValues(filteredColumns, dataTable.Rows[j]);
+            allRows.Add($"({string.Join(", ", rowValues)})");
+        }
+
+        insertQuery.Append(string.Join(", ", allRows));
+        insertQuery.Append(";");
+
+        return insertQuery;
+    }
+
+
+    private static List<string> PrepareRowValues(List<(string ColumnName, string DataType)> filteredColumns,
+        DataRow row)
+    {
+        var rowValues = new List<string>();
+
+        foreach (var column in filteredColumns)
+        {
+            var parameterValue = row[column.ColumnName];
+
+            // Преобразование строк и UUID
+            if (parameterValue is string || parameterValue is Guid)
+            {
+                // Проверяем, если строка пустая, для полей типа "time" ставим NULL
+                if (column.DataType == "time" && string.IsNullOrEmpty(parameterValue.ToString()))
+                {
+                    rowValues.Add("NULL");
+                }
+                else
+                {
+                    rowValues.Add($"'{parameterValue.ToString().Replace("'", "''")}'");
+                }
+            }
+            // Преобразуем дату в нужный формат
+            else if (parameterValue is DateTime dateTimeValue)
+            {
+                rowValues.Add($"'{dateTimeValue.ToString("yyyy-MM-dd HH:mm:ss")}'");
+            }
+            // Если тип данных - timestamp, ставим NULL
+            else if (column.DataType == "timestamp")
+            {
+                rowValues.Add("NULL");
+            }
+            // Преобразование для типа time
+            else if (column.DataType == "time")
+            {
+                if (parameterValue == DBNull.Value || string.IsNullOrEmpty(parameterValue.ToString()))
+                {
+                    rowValues.Add("NULL"); // Для пустых значений ставим NULL
+                }
+                else
+                {
+                    rowValues.Add($"'{parameterValue.ToString()}'"); // Преобразуем в формат time
+                }
+            }
+            // Преобразование bit в целое
+            else if (column.DataType == "bit")
+            {
+                rowValues.Add(parameterValue != DBNull.Value && (bool)parameterValue ? "1" : "0");
+            }
+            // Обработка числовых значений с точкой как разделителем дробной части
+            else if (parameterValue is double || parameterValue is float || parameterValue is decimal)
+            {
+                rowValues.Add(Convert.ToString(parameterValue, System.Globalization.CultureInfo.InvariantCulture));
+            }
+            // Обрабатываем NULL значения
+            else if (parameterValue == DBNull.Value)
+            {
+                rowValues.Add("NULL");
+            }
+            else
+            {
+                rowValues.Add(parameterValue.ToString());
+            }
+        }
+
+        return rowValues;
+    }
+
+    private static void ExecuteBatchInsert(NpgsqlConnection pgConnection, StringBuilder insertQuery)
+    {
+        using (var cmd = new NpgsqlCommand(insertQuery.ToString(), pgConnection))
+        {
+            Console.WriteLine(insertQuery.ToString());
+            cmd.ExecuteNonQuery();
+        }
+    }
 }

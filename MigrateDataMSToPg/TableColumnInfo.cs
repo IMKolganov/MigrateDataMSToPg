51669284595758585a81abae04ac3236710aa870
Ivan Kolganov <ikolganov@whitebox.ru>
2024-10-11 02:26:44 +0300

    removed older files

--- MigrateDataMSToPg/TableColumnInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MigrateDataMSToPg;
-
-public class TableColumnInfo
-{
-    public string TableName { get; set; }      // Имя таблицы
-    public string ColumnName { get; set; }     // Имя столбца
-    public string DataType { get; set; }       // Тип данных столбца
-    public bool IsNullable { get; set; }       // Может ли столбец быть NULL
-}
--- MigrateDataMSToPg/TableInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MigrateDataMSToPg;
-
-public class TableInfo
-{
-    public string TableName { get; set; }
-    public List<string> Columns { get; set; } = new List<string>();
-    public double SizeMB { get; set; }
-}
